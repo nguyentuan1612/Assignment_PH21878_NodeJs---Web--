6e56dd6c99ade632ef4cdfdd402d0fd5ee84ecee
nguyentuan1612 <tuanbeo203@gmail.com>
2023-03-26 00:16:21 +0700

    login and register req from client

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -7,30 +7,32 @@ class userController {
   updateUser(req, res) {
     res.render("suaNguoiDung");
   }
-  async login(req,res,next){
-    const {email,password} = await req.body;
-    User.findOne({email:email}).then(async (response) => {
+  async login(req, res, next) {
+    const { email, password } = await req.body;
+    User.findOne({ email: email }).then(async (response) => {
       const data = await MongooseToObject(response);
-      if(data){
-        if(data.email === email && data.password === password){
-          return res.status(200).json({message:'success'})
-        }else{
-          return res.status(404).json({message:'fail'})
+      if (data) {
+        if (data.email === email && data.password === password) {
+          return res.status(200).json({ message: 'success' ,data : data})
+        } else {
+          return res.status(404).json({ message: 'fail' })
         }
       }
     }).catch((error) => {
-      res.status(404).json({message:"server error"})
+      res.status(404).json({ message: "server error" })
     })
-   
+
   }
   store(req, res, next) {
     const dataUser = req.body;
-    dataUser.image = "";
+    dataUser.image = "https://ibiettuot.com/wp-content/uploads/2021/10/avatar-mac-dinh.png";
     const user = new User(dataUser);
     user.save()
-      .then(() => res.send("DONE"))
-      .catch((error) => next(error));
-    
+      .then(() => {
+        return res.status(201).json({ message: "created" })
+      })
+      .catch((error) => res.status(500).json({ message: "error" }));
+
     // res.json(dataUser)
   }
   // createUser(req,res){
