8b8019a072d48df437c83aff0cf472b7fbb4c6cf
nguyentuan1612 <tuanbeo203@gmail.com>
2023-03-27 13:55:11 +0700

    new changes

--- src/app/controllers/sanPhamController.js
+++ src/app/controllers/sanPhamController.js
@@ -1,12 +1,22 @@
 const Product = require("../models/Product");
 const { mutipleMongooseToObject } = require("../../util/mongoose");
 const store = require("store");
+
 class sanPhamController {
   goToDetail(req, res) {
     res.render("sanPhamDetail");
   }
   createProduct(req, res) {
     res.render("themSanPham");
+  }
+  store(req, res, next) {
+    const data = req.body;
+    data.image = "";
+    const product = new Product(data);
+    product
+      .save()
+      .then(() => res.redirect("themSanPham"))
+      .catch((error) => next(error));
   }
   index(req, res, next) {
     const name = store.get("nameAdminLogin");
